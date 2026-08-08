249867df6d0cdb4aa8d3c9fbadd9c4f97d8de18c
bt24btech11003-bit <bt24btech11003@iith.ac.in>
2026-08-08 12:45:06 +0530

    Configure server for deployment

--- server/index.js
+++ server/index.js
@@ -1,32 +1,36 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
-const indexRoute = require("./routes/routes")
-const authRoute = require("./routes/auth");
-
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const indexRoute = require("./routes/routes");
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 
-console.log(process.env);
-console.log(process.env.MONGO_URL);
+const app = express();
 
-const app = express()
 app.use(cors());
-const port = process.env.PORT || 8000
-
 app.use(express.json());
 
 app.use("/auth", authRoute);
 app.use("/", indexRoute);
-   
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => console.log("server started at the port,"+port))
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(" MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(" MongoDB Connection Failed");
+    console.error(err);
+  }
+}
+
+startServer();
