e25c0215b8f0ebc607ed931b84cb2abca46f1cf3
bt24btech11003-bit <bt24btech11003@iith.ac.in>
2026-08-21 15:55:41 +0530

    Change AI model from llama-3.3 to openai/gpt-oss-120b

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -3,7 +3,7 @@ const groq = require("../config/groq");
 const askAI = async (prompt) => {
 
     const response = await groq.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
+        model: "openai/gpt-oss-120b",
         messages: [
     {
         role: "user",
